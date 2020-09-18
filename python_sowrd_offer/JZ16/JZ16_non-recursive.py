29112b49809a793d13738bda017ab51f68219258
sususama5555 <876324872@qq.com>
2020-09-19 01:19:08 +0800

    refactor:NO.16 MergeLink non-recursive method

--- python_sowrd_offer/JZ16/JZ16_non-recursive.py
+++ python_sowrd_offer/JZ16/JZ16_non-recursive.py
@@ -10,35 +10,35 @@
 
 
 class ListNode:
-    """
-    非递归版本
-    """
     def __init__(self, x):
         self.val = x
         self.next = None
 
 
 class Solution:
+    """
+    非递归版本
+    """
     # 返回合并后列表
     def Merge(self, pHead1, pHead2):
         # write code here
-        pHeadList = []
+        phead = ListNode(0)
+        tmp = phead
         while pHead1 and pHead2:
             # 当一个链表遍历完后，退出循环
             if pHead1.val > pHead2.val:
-                pHeadList.append(pHead2)
+                tmp.next = pHead2
                 pHead2 = pHead2.next
             else:
-                pHeadList.append(pHead1)
+                tmp.next = pHead1
                 pHead1 = pHead1.next
+            tmp = tmp.next
         # 记录没遍历完的链表
         if not pHead1:
-            pHeadList.append(pHead2)
+            tmp.next = pHead2
         else:
-            pHeadList.append(pHead1)
-        for i in range(len(pHeadList) - 1):
-            pHeadList[i].next = pHeadList[i + 1]
-        return pHeadList[0]
+            tmp.next = pHead1
+        return phead.next
 
 
 # {1,3,5}
