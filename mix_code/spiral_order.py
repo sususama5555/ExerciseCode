977097d5910a527a1ab793d88a709eb2682bbb51
sususama5555 <876324872@qq.com>
2020-09-24 10:20:53 +0800

    NO.19:fit matrix like [[1],[2]...[n]]

--- mix_code/spiral_order.py
+++ mix_code/spiral_order.py
@@ -24,11 +24,18 @@ class SpiralOrder:
         # todo matrix为[]时，matrix[0]边界问题
         if matrix is None or m == 0 or n == 0:
             return order_list
+        if n == 1:
+            return [i[0] for i in matrix]
+        if m == 1:
+            return matrix[0]
         # 大循环，从外向内逐层遍历矩阵
         for i in range(int((min(m, n) + 1) / 2)):
             # 从左到右遍历 上边
             for j in range(i, n - i):
                 order_list.append(matrix[i][j])
+            # 当最后一层只剩一行时，遍历后结束其他三条边的统计
+            if m < n and m % 2 and i == int((m - 1) / 2):
+                break
             # 从上到下遍历 右边
             for j in range(i + 1, m - i):
                 order_list.append(matrix[j][(n - 1) - i])
@@ -55,7 +62,15 @@ test_matrix_2 = [
     [21, 22, 23, 24, 25]
 ]
 test_matrix_3 = [[x for x in range(6 * y, 6 * (y + 1))] for y in range(6)]
+test_matrix_4 = [
+    [1],
+    [2],
+    [3],
+    [4],
+    [5]
+]
 spiral_order = SpiralOrder()
-print(spiral_order.spiralOrder(test_matrix_1))
-print(spiral_order.spiralOrder(test_matrix_2))
-print(spiral_order.spiralOrder(test_matrix_3))
+# print(spiral_order.spiralOrder(test_matrix_1))
+# print(spiral_order.spiralOrder(test_matrix_2))
+# print(spiral_order.spiralOrder(test_matrix_3))
+print(spiral_order.spiralOrder(test_matrix_4))
--- python_sowrd_offer/JZ19/JZ19.py
+++ python_sowrd_offer/JZ19/JZ19.py
@@ -3,7 +3,10 @@
 @File  :JZ19.py
 @Author:Sapphire
 @Date  :2020/9/22 16:55
-@Desc  :
+@Desc  :顺时针打印矩阵：详解见./mix_code/spiral_order.py
+输入一个矩阵，按照从外向里以顺时针的顺序依次打印出每一个数字，
+例如，如果输入如下4 X 4矩阵： 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+则依次打印出数字1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10.
 """
 
 
@@ -21,11 +24,11 @@ class Solution:
         if m == 1:
             return matrix[0]
         for i in range(int((min(m, n) + 1) / 2)):
-            if m < n and m % 2 and i == int((m - 1) / 2):
-                order_list.append(x for x in matrix[i] if x in range())
-                break
             for j in range(i, n - i):
                 order_list.append(matrix[i][j])
+            if m < n and m % 2 and i == int((m - 1) / 2):
+                # 当最后一层只剩一行时，遍历后结束其他三条边的统计
+                break
             for j in range(i + 1, m - i):
                 order_list.append(matrix[j][(n - 1) - i])
             for j in range(i + 1, n - i):
@@ -51,5 +54,4 @@ test_matrix_2 = [
 test_matrix_3 = [[x for x in range(6 * y, 6 * (y + 1))] for y in range(6)]
 spiral_order = Solution()
 print(spiral_order.printMatrix(test_matrix_1))
-# print(spiral_order.printMatrix(test_matrix_2))
-# print(spiral_order.printMatrix(test_matrix_3))
+print(spiral_order.printMatrix(test_matrix_2))
