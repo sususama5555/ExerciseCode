73f3f50c84885b779a38107ae94042bcfd103947
sususama5555 <876324872@qq.com>
2020-09-25 16:46:05 +0800

    mix code:Python Serialization

---
+++ mix_code/Serialization.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@File  :Serialization.py
+@Author:Sapphire
+@Date  :2020/9/25 15:09
+@Desc  :python序列化
+Python语言特定的序列化模块是pickle，但如果要把序列化搞得更通用、更符合Web标准，就可以使用json模块。
+序列化类的实例：
+无法把Student类实例序列化为JSON，是因为默认情况下，dumps()方法不知道如何将Student实例变为一个JSON的{}对象
+可通过default参数传入实例的序列化方式，例如 default=lambda obj: obj.__dict__
+"""
+import json
+import pickle
+
+
+class Student(object):
+    def __init__(self, name, age, hobbit):
+        self.name = name
+        self.age = age
+        self.hobbit = hobbit
+
+    def introduction(self):
+        print("My name is {} , i`m  {} years old , i like {}".format(self.name, self.age, self.hobbit))
+
+
+student = Student("sapphire", 12, "写代码")
+
+print(dir(student))
+print(repr(student))
+print(student.__dict__)
+print(json.dumps(student.__dict__))
+print(json.dumps(student, default=lambda obj: obj.__dict__))
+print(pickle.dumps(student.__dict__))
