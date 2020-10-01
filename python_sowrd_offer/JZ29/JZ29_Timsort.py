ac8cb3975bd704077c5445459d659ff3bca5e2be
sususama5555 <876324872@qq.com>
2020-10-02 00:02:47 +0800

    NO.29:GetLeastKNumbers with Timsort

---
+++ python_sowrd_offer/JZ29/JZ29_Timsort.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@File  :JZ29.py
+@Author:Sapphire
+@Date  :2020/10/1 23:59
+@Desc  :最小的K个数
+Timsort：蒂姆排序：
+使用python内置的tim排序（底层是快排等）
+不推荐使用
+todo 堆排序：循环列表，把最小的K个数放在堆里面
+"""
+
+
+class Solution:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        # write code here
+        if not tinput or k > len(tinput):
+            return []
+        tinput.sort()
+        return tinput[:k]
