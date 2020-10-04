89bd42e6d768a15e7cab154f518fa204c67267a5
sususama5555 <876324872@qq.com>
2020-10-04 22:52:21 +0800

    NO.30:add annotation

--- python_sowrd_offer/JZ30/JZ30_simple.py
+++ python_sowrd_offer/JZ30/JZ30_simple.py
@@ -23,6 +23,14 @@ class Solution:
             global_max = max(global_max, local_max)
         return global_max
 
+        # 另一种：动态规划
+        # n = len(array)
+        # dp = [i for i in array]
+        # for i in range(1, n):
+        #     dp[i] = max(dp[i - 1] + array[i], array[i])
+        #
+        # return max(dp)
+
 
 array = [1, -2, 3, 10, -4, 7, 2, -5]
 solution = Solution()
