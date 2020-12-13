4457ae8d264716797a3a14612d8bad6a30b18cf7
ryanshu <876324872@qq.com>
2020-12-14 00:41:00 +0800

    sort:selection_sort选择排序

---
+++ basis_algorithms/sort/selection_sort.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@File  :selection_sort.py
+@Author:Sapphire
+@Date  :2020/12/13 21:34
+@Desc  :选择排序：
+时间复杂度：O(n2)
+空间复杂度：O(1)
+"""
+
+
+def selection_sort(array):
+    """
+    选择排序：
+    第一次从待排序的数据元素中选出最小（或最大）的一个元素，存放在序列的起始位置，
+    然后再从剩余的未排序元素中寻找到最小（大）元素，然后放到已排序的序列的末尾。
+    以此类推，直到全部待排序的数据元素的个数为零。
+    """
+    loop = len(array) - 1
+    for i in range(loop):
+        min_index = i
+        for j in range(i, loop - 1):
+            if array[j] < array[min_index]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
+
+
+array = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48]
+print(selection_sort(array))
